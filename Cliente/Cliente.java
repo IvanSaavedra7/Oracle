6b6f1f2ffaf9883a26e83f0de33495d88a6554d3
aluno <aluno@dinalva32.academico.cotuca.unicamp.br>
2020-08-28 13:05:02 -0300

    primeiro commit rafael

--- Cliente/Cliente.java
+++ Cliente/Cliente.java
@@ -1,7 +1,7 @@
 import java.net.*;
 import java.io.*;
 
-public class Cliente
+public class Cliente // TESTE - RAFAEL
 {
 	public static final String HOST_PADRAO  = "localhost";
 	public static final int    PORTA_PADRAO = 3000;
